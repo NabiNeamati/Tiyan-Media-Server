a94947d55c00d08741a11f51ac4fc02042903dff
Nabi Neamati <neamati883@gmail.com>
2020-03-31 18:32:14 +0430

    Configuration Step 1

--- install.sh
+++ install.sh
@@ -10,21 +10,21 @@ mkdir ~/working/ngx_devel_kit
 mkdir ~/working/set-misc-nginx-module
 mkdir ~/working/nginx
 mkdir ~/working/nginx-hmac-secure-link
-git clone https://github.com/upggr/IELKO-MEDIA-SERVER.git ~/working/IELKO
+git clone https://github.com/NabiNeamati/Tiyan-Media-Server.git ~/working/Tiyan
 git clone https://github.com/sergey-dryabzhinsky/nginx-rtmp-module.git ~/working/nginx-rtmp-module
 git clone https://github.com/openresty/set-misc-nginx-module.git ~/working/set-misc-nginx-module
 git clone https://github.com/simpl/ngx_devel_kit.git ~/working/ngx_devel_kit
 git clone https://github.com/nginx-modules/nginx-hmac-secure-link.git ~/working/nginx-hmac-secure-link
-wget http://nginx.org/download/nginx-1.13.6.tar.gz -P ~/working
-tar -xf ~/working/nginx-1.13.6.tar.gz -C ~/working/nginx --strip-components=1
-rm ~/working/nginx-1.13.6.tar.gz
+wget http://nginx.org/download/nginx-1.16.1.tar.gz -P ~/working
+tar -xf ~/working/nginx-1.16.1.tar.gz -C ~/working/nginx --strip-components=1
+rm ~/working/nginx-1.16.1.tar.gz
 cd ~/working/nginx
 #./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module --add-module=../nginx-hmac-secure-link
-./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module
+./configure --with-http_ssl_module --add-module=../nginx-rtmp-module --add-module=../ngx_devel_kit --add-module=../set-misc-nginx-module --with-debug  --with-cc-opt="-Wimplicit-fallthrough=0"
 make -j 2
 sudo make install
-cp ~/working/IELKO/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
-cp ~/working/IELKO/conf/nginx.service /etc/init.d/nginx
+cp ~/working/Tiyan/conf/nginx.conf /usr/local/nginx/conf/nginx.conf
+cp ~/working/Tiyan/conf/nginx.service /etc/init.d/nginx
 sudo chmod +x /etc/init.d/nginx
 sudo /usr/sbin/update-rc.d -f nginx defaults
 ufw allow 8080
@@ -34,11 +34,11 @@ iptables -I INPUT -p tcp --dport 80 -j ACCEPT
 iptables -I INPUT -p tcp --dport 1935 -j ACCEPT
 iptables -I INPUT -p tcp --dport 8080 -j ACCEPT
 rm /usr/local/nginx/html/*
-cp ~/working/IELKO/www/index.php /usr/local/nginx/html/index.php
-cp ~/working/IELKO/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
-cp ~/working/IELKO/www/stream.xml /usr/local/nginx/html/stream.xml
-cp ~/working/IELKO/www/testing.png /usr/local/nginx/html/testing.png
-cp ~/working/IELKO/www/favicon.ico /usr/local/nginx/html/favicon.ico
+cp ~/working/Tiyan/www/index.php /usr/local/nginx/html/index.php
+cp ~/working/Tiyan/www/ielko-media-server.css /usr/local/nginx/html/ielko-media-server.css
+cp ~/working/Tiyan/www/stream.xml /usr/local/nginx/html/stream.xml
+cp ~/working/Tiyan/www/testing.png /usr/local/nginx/html/testing.png
+cp ~/working/Tiyan/www/favicon.ico /usr/local/nginx/html/favicon.ico
 git clone https://github.com/upggr/ielko-video-player /usr/local/nginx/html/player
 ip=$(curl -s checkip.dyndns.org | sed -e 's/.*Current IP Address: //' -e 's/<.*$//' )
 sed -i -- 's/replaceip/'"$ip"'/g' /usr/local/nginx/html/stream.xml
